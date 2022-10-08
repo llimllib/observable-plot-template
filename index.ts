dfce1b3c530b8d2d61fbaaafb5555f554e59ecd7
Bill Mill <bill@billmill.org>
2022-10-07 22:37:56 -0400

    better initial chart

--- index.ts
+++ index.ts
@@ -1,15 +1,23 @@
 import * as Plot from "@observablehq/plot";
 
 async function main(): Promise<void> {
-  // const res = await fetch(`data.json`);
-  // const stats = await res.json();
-  const dotplot = Plot.dot(data, {
-    x: "usg_pct",
-    y: "ts_pct",
-    stroke: "team",
-    title: "name",
-  }).plot();
-  document.querySelector("#plot")!.append(dotplot);
+  const barchart = Plot.plot({
+    grid: true,
+    color: {
+      domain: [52, 62],
+      pivot: 55,
+      scheme: "BuRd",
+    },
+    marks: [
+      Plot.barY(data, {
+        x: "name",
+        y: "playoff_ts_pct",
+        fill: "playoff_ts_pct",
+      }),
+      Plot.ruleY([0]),
+    ],
+  });
+  document.querySelector("#plot")!.append(barchart);
 }
 
 window.addEventListener("DOMContentLoaded", async (_evt) => {
@@ -18,43 +26,63 @@ window.addEventListener("DOMContentLoaded", async (_evt) => {
 
 const data = [
   {
-    name: "Precious Achiuwa",
-    team: "TOR",
-    usg_pct: 18.5,
-    ts_pct: 50.3,
-    playoff_mp: 167,
-    playoff_usg_pct: 18.2,
-    playoff_ts_pct: 54.1,
-    playoff_mpg: 27.83,
+    name: "Stephen Curry",
+    team: "GSW",
+    usg_pct: 30.8,
+    ts_pct: 60.099999999999994,
+    playoff_mp: 150,
+    playoff_usg_pct: 37,
+    playoff_ts_pct: 64.1,
+    playoff_mpg: 30,
   },
   {
-    name: "Steven Adams",
+    name: "Ja Morant",
     team: "MEM",
-    usg_pct: 12,
-    ts_pct: 56.00000000000001,
-    playoff_mp: 31,
-    playoff_usg_pct: 1.3,
-    playoff_ts_pct: null,
-    playoff_mpg: 10.33,
+    usg_pct: 33.7,
+    ts_pct: 57.49999999999999,
+    playoff_mp: 222,
+    playoff_usg_pct: 28.4,
+    playoff_ts_pct: 49.6,
+    playoff_mpg: 37,
   },
   {
-    name: "Bam Adebayo",
-    team: "MIA",
-    usg_pct: 25,
-    ts_pct: 60.8,
-    playoff_mp: 157,
-    playoff_usg_pct: 18.9,
-    playoff_ts_pct: 60,
-    playoff_mpg: 31.4,
+    name: "Devin Booker",
+    team: "PHO",
+    usg_pct: 32,
+    ts_pct: 57.599999999999994,
+    playoff_mp: 98,
+    playoff_usg_pct: 27.9,
+    playoff_ts_pct: 64.5,
+    playoff_mpg: 32.67,
   },
   {
-    name: "Nickeil Alexander-Walker",
-    team: "UTA",
-    usg_pct: 24.1,
-    ts_pct: 47.5,
-    playoff_mp: 5,
-    playoff_usg_pct: 22.9,
-    playoff_ts_pct: 102.49999999999999,
-    playoff_mpg: 5,
+    name: "Kevin Durant",
+    team: "BRK",
+    usg_pct: 31.2,
+    ts_pct: 63.4,
+    playoff_mp: 176,
+    playoff_usg_pct: 31.5,
+    playoff_ts_pct: 52.6,
+    playoff_mpg: 44,
+  },
+  {
+    name: "Jayson Tatum",
+    team: "BOS",
+    usg_pct: 32.1,
+    ts_pct: 57.8,
+    playoff_mp: 167,
+    playoff_usg_pct: 30.7,
+    playoff_ts_pct: 61.6,
+    playoff_mpg: 41.75,
+  },
+  {
+    name: "James Harden",
+    team: "PHI",
+    usg_pct: 27.2,
+    ts_pct: 58.3,
+    playoff_mp: 243,
+    playoff_usg_pct: 23,
+    playoff_ts_pct: 58.199999999999996,
+    playoff_mpg: 40.5,
   },
 ];
